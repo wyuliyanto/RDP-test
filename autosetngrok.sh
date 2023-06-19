fd4df7fd805baa66ddc8ca27b10e651e7dfc6063
wyuliyanto <93317023+wyuliyanto@users.noreply.github.com>
2023-06-19 14:04:13 +0700

    Update autosetngrok.sh

--- autosetngrok.sh
+++ autosetngrok.sh
@@ -4,4 +4,4 @@ echo "Auto Setup Ngrok Token"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-./ngrok authtoken 2G9yhRl26PoUl6A6K9rpDp5LVLs_3K7YKniK1jPe15Cs4YYoj #put your authtoken, not my token >:3
+./ngrok authtoken 2RN5M6LSBHQxwx6HZ6lxNfYypu2_2k9aww5B3RU2YJVRgDouf #put your authtoken, not my token >:3
